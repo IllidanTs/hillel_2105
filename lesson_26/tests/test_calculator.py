397bf1af385df8c236cebc710f6de531b281c178
Elias Tsatsorin <itsatsorin@gmail.com>
2024-09-15 20:35:18 -0400

    Leeeeeroy Jenkins

---
+++ lesson_26/tests/test_calculator.py
@@ -0,0 +1,10 @@
+import pytest
+from calculator import add, subtract
+
+def test_add():
+    assert add(2, 3) == 5
+    assert add(-1, 1) == 0
+
+def test_subtract():
+    assert subtract(5, 3) == 2
+    assert subtract(0, 1) == -1
